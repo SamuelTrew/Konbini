6ed77f67a09107f216a9c13101c7fbf963328c31
Samuel Trew <samuel.trew@outlook.com>
2024-08-10 23:34:26 +0100

    Added linting and formatted whole codebase

--- app.config.ts
+++ app.config.ts
@@ -1,22 +1,22 @@
-import { ExpoConfig, ConfigContext } from '@expo/config';
-import * as dotenv from 'dotenv';
+import { ExpoConfig, ConfigContext } from "@expo/config"
+import * as dotenv from "dotenv"
 
 // initialize dotenv
-dotenv.config();
+dotenv.config()
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
    ...config,
-   "name": "konbini",
-   "slug": "konbini",
-   "version": "0.0.1",
-   "orientation": "portrait",
-   "icon": "./assets/images/icon.png",
-   "scheme": "myapp",
-   "userInterfaceStyle": "automatic",
-   "splash": {
-      "image": "./assets/images/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+   name: "konbini",
+   slug: "konbini",
+   version: "0.0.1",
+   orientation: "portrait",
+   icon: "./assets/images/icon.png",
+   scheme: "myapp",
+   userInterfaceStyle: "automatic",
+   splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
    },
    ios: {
       supportsTablet: true,
@@ -25,42 +25,43 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       },
    },
    android: {
-      "adaptiveIcon": {
-         "foregroundImage": "./assets/images/adaptive-icon.png",
-         "backgroundColor": "#ffffff"
+      adaptiveIcon: {
+         foregroundImage: "./assets/images/adaptive-icon.png",
+         backgroundColor: "#ffffff",
       },
       config: {
          googleMaps: {
             apiKey: process.env.GOOGLE_CLOUD_API_KEY,
          },
       },
-      package: "dev.trew.konbini"
+      package: "dev.trew.konbini",
    },
-   "web": {
-      "bundler": "metro",
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
+   web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
    },
-   "plugins": [
+   plugins: [
       "expo-router",
       [
          "expo-location",
          {
-            locationWhenInUsePermission: "Allow $(PRODUCT_NAME) to use your location. Necessary for app to function.",
+            locationWhenInUsePermission:
+               "Allow $(PRODUCT_NAME) to use your location. Necessary for app to function.",
             isAndroidForegroundServiceEnabled: true,
-         }
-      ]
+         },
+      ],
    ],
-   "experiments": {
-      "typedRoutes": true
+   experiments: {
+      typedRoutes: true,
    },
-   "extra": {
-      "router": {
-         "origin": false
+   extra: {
+      router: {
+         origin: false,
       },
-      "eas": {
-         "projectId": "c0c0afda-bfa1-4874-a9d6-a9f5b1af3f6b"
-      }
+      eas: {
+         projectId: "c0c0afda-bfa1-4874-a9d6-a9f5b1af3f6b",
+      },
    },
-   "owner": "saminxe"
-});
+   owner: "saminxe",
+})
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,64 +1,80 @@
-import { Image, StyleSheet, Platform, View } from 'react-native';
+import { Image, StyleSheet, Platform, View } from "react-native"
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import MapView, {MarkerAnimated, PROVIDER_GOOGLE} from 'react-native-maps';
+import { HelloWave } from "@/components/HelloWave"
+import ParallaxScrollView from "@/components/ParallaxScrollView"
+import { ThemedText } from "@/components/ThemedText"
+import { ThemedView } from "@/components/ThemedView"
+import MapView, { MarkerAnimated, PROVIDER_GOOGLE } from "react-native-maps"
 
-import * as Location from 'expo-location';
-import { useEffect, useState } from 'react';
-
+import * as Location from "expo-location"
+import { useEffect, useState } from "react"
 
 export default function HomeScreen() {
-  const [errMessage, setErr] = useState<string>()
-  const [location, setLocation] = useState<Location.LocationObject>()
+   const [errMessage, setErr] = useState<string>()
+   const [location, setLocation] = useState<Location.LocationObject>()
 
-  const requestLocationPermission = async () => {
-    const result = await Location.requestForegroundPermissionsAsync()
-    if (result.granted) {
-      setLocation(await Location.getCurrentPositionAsync({
-        accuracy: Location.LocationAccuracy.BestForNavigation,
-        timeInterval: 1000,
-        distanceInterval: 5
-      }))
-      setErr(undefined)
-      return
-    }
+   const requestLocationPermission = async () => {
+      const result = await Location.requestForegroundPermissionsAsync()
+      if (result.granted) {
+         setLocation(
+            await Location.getCurrentPositionAsync({
+               accuracy: Location.LocationAccuracy.BestForNavigation,
+               timeInterval: 1000,
+               distanceInterval: 5,
+            }),
+         )
+         setErr(undefined)
+         return
+      }
 
-    setErr("Permission to access location was denied. Please enable to allow the app to function.")
-  }
+      setErr(
+         "Permission to access location was denied. Please enable to allow the app to function.",
+      )
+   }
 
-  useEffect(() => {
-    void requestLocationPermission()
-  }, [])
+   useEffect(() => {
+      void requestLocationPermission()
+   }, [])
 
-  return (
-    <ThemedView style={styles.container}>
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{latitude: 35.652832, longitude: 139.839478, latitudeDelta: 35.652832, longitudeDelta: 139.839478}}
-        // initialRegion={location && {latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: location.coords.latitude, longitudeDelta: location.coords.longitude}}
-        camera={{zoom: 10, center: {latitude: 35.65, longitude: 139.75}, heading: 0, pitch: 0}}
-      >
-        {location && <MarkerAnimated coordinate={{latitude: 35.652832, longitude: 139.839478}} />}
-      </MapView>
-      {errMessage && <ThemedText>{errMessage}</ThemedText>}
-      <ThemedText>{JSON.stringify(location, null, 2)}</ThemedText>
-    </ThemedView>
-  );
+   return (
+      <ThemedView style={styles.container}>
+         <MapView
+            style={styles.map}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={{
+               latitude: 35.652832,
+               longitude: 139.839478,
+               latitudeDelta: 35.652832,
+               longitudeDelta: 139.839478,
+            }}
+            // initialRegion={location && {latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: location.coords.latitude, longitudeDelta: location.coords.longitude}}
+            camera={{
+               zoom: 10,
+               center: { latitude: 35.65, longitude: 139.75 },
+               heading: 0,
+               pitch: 0,
+            }}
+         >
+            {location && (
+               <MarkerAnimated
+                  coordinate={{ latitude: 35.652832, longitude: 139.839478 }}
+               />
+            )}
+         </MapView>
+         {errMessage && <ThemedText>{errMessage}</ThemedText>}
+         <ThemedText>{JSON.stringify(location, null, 2)}</ThemedText>
+      </ThemedView>
+   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 10,
-    marginVertical: 30,
-    
-  },
-  map: {
-    width: '100%',
-    height: '70%',
-  },
-});
+   container: {
+      flex: 1,
+      marginHorizontal: 10,
+      marginVertical: 30,
+   },
+   map: {
+      width: "100%",
+      height: "70%",
+   },
+})
